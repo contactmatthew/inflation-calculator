c42e5ffad04ea19d8fa5b78029ba36f0dfa45513
contactmatthew <your-email@example.com>
2025-12-31 14:25:40 +0800

    Add favicon to website

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inflation Calculator - Calculate Inflation Across Countries</title>
     
+    <link rel="icon" type="image/png" href="favicon.png">
+    <link rel="shortcut icon" type="image/png" href="favicon.png">
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     
     <script src="https://cdn.tailwindcss.com"></script>
